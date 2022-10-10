975c64cd75c9f02c58699402b33c3f91f8207cc0
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-11 00:09:37 +0600

    navbar complete

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/statistic'>Statistic</Link>
-            <Link to='/blog'>Blog</Link>
-        </nav>
+        <div className='navContaine'>
+            <div>
+                <h2>National Quiz</h2>
+            </div>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/statistic'>Statistic</Link>
+                <Link to='/blog'>Blog</Link>
+            </nav>
+        </div>
     );
 };
 
