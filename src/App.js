ca78866fdea396b73eaa7498c01d21ce863d222f
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-10 23:16:47 +0600

    something rong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Body from './Component/Body/Body';
+import Main from './Component/Main/Main';
 
 function App() {
+  const router =createBrowserRouter([
+    {
+      path: '/',
+      Element: <Main></Main>,
+      children:[
+        {
+          path: '/',
+          element: <Body></Body>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <h1>Assignment start</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
