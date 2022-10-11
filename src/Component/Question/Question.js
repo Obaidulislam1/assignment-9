e793e24a6554be1d73b60012662de1320e0a2d91
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-11 19:53:32 +0600

    question show complete

--- src/Component/Question/Question.js
+++ src/Component/Question/Question.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Option from '../Option/Option';
 
 const Question = ({question}) => {
     console.log(question)
@@ -6,7 +7,9 @@ const Question = ({question}) => {
     return (
         <div>
             <h1>Question:{question.question}</h1>
-
+            {
+                options.map(option =><Option key={option.id} option={option}></Option>)
+            }
         </div>
     );
 };
