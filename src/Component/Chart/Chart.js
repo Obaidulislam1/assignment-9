a0ee0253f25b17974cba1047cc82a0c4277d8edf
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-12 14:06:22 +0600

    chart start

--- src/Component/Chart/Chart.js
+++ src/Component/Chart/Chart.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const Chart = ({info}) => {
-    const{total} = info;
-    console.log(total);
+    const{total,name} = info;
     return (
         <div>
-           
+           <LineChart width={500} height={400} data={info}>
+           <Line type="monotone" dataKey={total} stroke="#82ca9d" />
+           <XAxis />
+          <YAxis dataKey={name}/>
+           </LineChart>
         </div>
     );
 };
